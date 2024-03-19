c68099775a518ca00699c261e8fcfbb35c87e327
saadahmed12190 <160844785+saadahmed12190@users.noreply.github.com>
2024-03-19 13:50:41 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Define a route for the homepage
 app.get('/', (req, res) => {
-  res.send('<h1>Hi, my name is Saad and this is a test cluster 222222!</h1>');
+  res.send('<h1>Hi, my name is Saad and this is a test cluster 3333333!</h1>');
 });
 
 // Define a route for a sample API endpoint
